4e2b46b3461d86a94d0516c7a4feef28735bb8c8
Michel <221404137+It-is-Michel@users.noreply.github.com>
2025-08-15 22:14:26 -0300

    chore: Underline calculator object

---
+++ script.js
@@ -0,0 +1,17 @@
+function Calculator() {
+  // press button method
+
+  // current entry
+  // operator
+  // previous entry
+
+  // add method
+  // subtract method
+  // multiply method
+  // divide method
+
+  // get result method
+
+  // clear entry method
+  // clear all method
+}
